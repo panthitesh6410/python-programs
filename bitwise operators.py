d2af9c15ebb794e6c5a9ad137badffea664d7e1e
panthitesh6410 <panthitesh6410@gmail.com>
2019-11-02 09:18:20 +0530

    adding bitwise operators.py

---
+++ bitwise operators.py
@@ -0,0 +1,22 @@
+# BITWISE OPERAORS :
+
+# complement operator (~):
+print(~12)
+
+# bitwise AND operator (&) :
+print(12 & 13)      # if both are 1 then only it gives 1 in binary .
+
+# bitwise OR operator (|) :
+print(12 | 13)      # if any one is 1 then it gives 1 in binary .
+
+# bitwise XOR operator () :
+# if bith are different in binary (like 01 or 10) then it gives 1 else it gives 0.
+print(12 ^ 13)
+
+# left shift operator (<<) :
+print(10 << 2)
+# 1010.000  ---->  101000.0
+
+# right shift operator (>>) :
+print(10 >> 2)
+# 1010.000  ----->  10.000
--- untitled0.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov  1 21:38:54 2019
-
-@author: gwc
-"""
-
