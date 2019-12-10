64b491a4bc5bd741709954be563bc2a88e8d2175
panthitesh6410 <panthitesh6410@gmail.com>
2019-12-10 19:23:56 +0530

    basics of python upto conditional statments

---
+++ elif-statement.py
@@ -0,0 +1,10 @@
+marks = int(input("enter your marks : "))
+
+if marks >= 90:
+    print("A grade")
+elif marks >=70:
+    print("B grade")
+elif marks >=60:
+    print("C grade")
+elif marks < 60:
+    print("D grade")
---
+++ simple_interest.py
@@ -0,0 +1,11 @@
+p = input("Enter the principle value : ")
+r = input("enter the rate value : ")
+t = input("enter time value : ")
+
+p = int(p)
+r = int(r)
+t = int(t)
+
+si = (p * r * t) / 100
+
+print("Simple Interest is : ", si)
